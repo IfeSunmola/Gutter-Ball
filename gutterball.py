55f6bbfa42bcef7707a6fe3ef6dade6eb4df697c
Ife-PopOs <sunmolaife@gmail.com>
2022-11-10 20:54:57 -0700

    Added level text

--- gutterball.py
+++ gutterball.py
@@ -15,6 +15,9 @@ pygame.display.set_icon(window_icon)
 pygame.key.set_repeat(1, 4)
 clock = pygame.time.Clock()
 
+# font to use for all the text
+text_font = pygame.font.Font('fonts/digital-dream/DIGITALDREAM.ttf', 30)
+
 #Variables
 
 menu_surf = pygame.image.load("graphics/menu.png").convert()
@@ -23,6 +26,8 @@ background_surf = pygame.image.load("graphics/background.png").convert()
 
 player_surf = pygame.image.load('graphics/player.png').convert_alpha()
 player_rect = player_surf.get_rect(center=(50, 200))
+
+level_surf = text_font.render("LEVEL 1", False, 'green')  # play with the 2nd param to see what it does
 
 #Main_menu
 
@@ -75,8 +80,11 @@ def levels():
 
         if game_active:
             screen.blit(background_surf, (0, 0))
-            pygame.draw.rect(screen, "Black", pygame.Rect(0, 0, 400, 50))
-            pygame.draw.rect(screen, "Black", pygame.Rect(0, 350, 400, 50))
+
+            pygame.draw.rect(screen, "Black", pygame.Rect(0, 0, 400, 50))  # black space above the 'play ground'
+            screen.blit(level_surf, (120, 0))  # Shows level number
+
+            pygame.draw.rect(screen, "Black", pygame.Rect(0, 350, 400, 50))  # black space below the 'play ground'
             screen.blit(player_surf, player_rect)
 
         pygame.display.update()
