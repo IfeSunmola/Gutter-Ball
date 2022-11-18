40d3e4bc36d1fd088d00d1d2a3c2aa5efc5c1144
Ife-PopOs <sunmolaife@gmail.com>
2022-11-17 22:11:57 -0700

    Updated Isra's code that adds pins to the base of window

--- gutterball.py
+++ gutterball.py
@@ -7,10 +7,9 @@ WINDOW_HEIGHT = 400
 WINDOW_LENGTH = 400
 NUM_LEVELS = 6
 
-
 # Game initialization
 pygame.init()
-screen = pygame.display.set_mode((WINDOW_LENGTH,WINDOW_HEIGHT))
+screen = pygame.display.set_mode((WINDOW_LENGTH, WINDOW_HEIGHT))
 
 pygame.display.set_caption("GutterBall")
 
@@ -21,15 +20,10 @@ pygame.key.set_repeat(1, 4)
 clock = pygame.time.Clock()
 
 # More constants, these depend on pygame being initialized first, so they can't be with the constants above
-TEXT_FONT = pygame.font.Font('fonts/digital-dream/DIGITALDREAM.ttf', 30)  #font to use for all the text
+TEXT_FONT = pygame.font.Font('fonts/digital-dream/DIGITALDREAM.ttf', 30)  # font to use for all the text
 MENU_SURF = pygame.image.load("graphics/menu.png").convert()
 BACKGROUND_SURF = pygame.image.load("graphics/background.png").convert()
 PLAYER_SURF = pygame.image.load('graphics/player.png').convert_alpha()
-PIN1_SURF = pygame.image.load('graphics/pin1.png').convert_alpha()
-PIN2_SURF = pygame.image.load('graphics/pin2.png').convert_alpha()
-PIN3_SURF = pygame.image.load('graphics/pin3.png').convert_alpha()
-PIN4_SURF = pygame.image.load('graphics/pin4.png').convert_alpha()
-PIN5_SURF = pygame.image.load('graphics/pin5.png').convert_alpha()
 
 
 def level_init(level_num, player_rect):
@@ -49,27 +43,39 @@ def level_init(level_num, player_rect):
 
     return pin_rect
 
+
 def check_boundary(player_rect):
     if player_rect.left <= 0:
         player_rect.left = 0
-    elif player_rect.right >= WINDOW_LENGTH: 
+    elif player_rect.right >= WINDOW_LENGTH:
         player_rect.right = WINDOW_LENGTH
     elif player_rect.top <= WINDOW_HEIGHT - 350:  # TODO: Change to be relative to WINDOW_SIZE
         player_rect.top = 50
     elif player_rect.bottom >= (WINDOW_HEIGHT - 50):
         player_rect.bottom = (WINDOW_HEIGHT - 50)
 
+
+def draw_bottom_pins(level_num):
+    # the x position moves by 32px, starting from 350, so we can just keep subtracting
+    pos_x, pos_y = 350, 350
+    i = 2
+    while i <= level_num:
+        temp_surf = pygame.image.load(f'graphics/pin{i - 1}.png')
+        screen.blit(temp_surf, (pos_x, pos_y))
+        pos_x -= 32
+        i += 1
+
+
 def main():
     player_rect = PLAYER_SURF.get_rect(center=(50, 200))
 
-# State variables -> to keep track of what actions to perform in the game
+    # State variables -> to keep track of what actions to perform in the game
     game_active = True
     start_game = False
     level_num = 1
-    pos_y = 350
 
     while game_active:
-        screen.blit(MENU_SURF, (0,0))
+        screen.blit(MENU_SURF, (0, 0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -93,7 +99,7 @@ def main():
 
         if game_active and start_game:  # we only want to show the levels if start_game is true
             screen.blit(BACKGROUND_SURF, (0, 0))
-            
+
             if level_num > NUM_LEVELS:  # No levels left, exit
                 print("Game Finished")  # Do what happens when the game finishes
                 game_active = False
@@ -106,26 +112,7 @@ def main():
                     print("Strike!")  # Just so I can see something in the console, delete later
                     level_num += 1  # move to next level
 
-                if level_num == 2: #prints the appropriate pins for each level.
-                    screen.blit(PIN1_SURF,(350,pos_y))
-                if level_num == 3:
-                    screen.blit(PIN1_SURF,(318,pos_y))
-                    screen.blit(PIN2_SURF,(350,pos_y))
-                if level_num == 4:
-                    screen.blit(PIN1_SURF,(286,pos_y))
-                    screen.blit(PIN2_SURF,(318,pos_y))
-                    screen.blit(PIN3_SURF,(350,pos_y))
-                if level_num == 5:
-                    screen.blit(PIN1_SURF,(254,pos_y))
-                    screen.blit(PIN2_SURF,(286,pos_y))
-                    screen.blit(PIN3_SURF,(318,pos_y))
-                    screen.blit(PIN4_SURF,(350,pos_y))
-                if level_num == 6:
-                    screen.blit(PIN1_SURF,(222,pos_y))
-                    screen.blit(PIN2_SURF,(254,pos_y))
-                    screen.blit(PIN3_SURF,(286,pos_y))
-                    screen.blit(PIN4_SURF,(318,pos_y))
-                    screen.blit(PIN5_SURF,(350,pos_y))
+                draw_bottom_pins(level_num)
 
         pygame.display.update()
         clock.tick(60)
